7ff4d64c85b2ec8ef4087a0793d442c1acd3d778
Valentine AKEM <valyakem@yahoo.co.uk>
2022-08-03 09:52:01 -0400

    modification added

--- main.tf
+++ main.tf
@@ -109,142 +109,142 @@ resource "aws_security_group" "vpc_tls" {
 # VPC Endpoints Module (Add as required)
 ################################################################################
 
-# module "vpc_endpoints" {
-#   source = "../../modules/vpc-endpoints"
+module "vpc_endpoints" {
+  source = "./modules/vpc-endpoints"
 
-#   vpc_id             = module.vpc.vpc_id
-#   security_group_ids = [data.aws_security_group.default.id]
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [data.aws_security_group.default.id]
 
-#   endpoints = {
-#     s3 = {
-#       service = "s3"
-#       tags    = { Name = "s3-vpc-endpoint" }
-#     },
-#     dynamodb = {
-#       service         = "dynamodb"
-#       service_type    = "Gateway"
-#       route_table_ids = flatten([module.vpc.intra_route_table_ids, module.vpc.private_route_table_ids, module.vpc.public_route_table_ids])
-#       policy          = data.aws_iam_policy_document.dynamodb_endpoint_policy.json
-#       tags            = { Name = "dynamodb-vpc-endpoint" }
-#     },
-#     ssm = {
-#       service             = "ssm"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#       security_group_ids  = [aws_security_group.vpc_tls.id]
-#     },
-#     ssmmessages = {
-#       service             = "ssmmessages"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#     lambda = {
-#       service             = "lambda"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#     ecs = {
-#       service             = "ecs"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#     ecs_telemetry = {
-#       create              = false
-#       service             = "ecs-telemetry"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#     ec2 = {
-#       service             = "ec2"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#       security_group_ids  = [aws_security_group.vpc_tls.id]
-#     },
-#     ec2messages = {
-#       service             = "ec2messages"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#     ecr_api = {
-#       service             = "ecr.api"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#       policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
-#     },
-#     ecr_dkr = {
-#       service             = "ecr.dkr"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#       policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
-#     },
-#     kms = {
-#       service             = "kms"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#       security_group_ids  = [aws_security_group.vpc_tls.id]
-#     },
-#     codedeploy = {
-#       service             = "codedeploy"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#     codedeploy_commands_secure = {
-#       service             = "codedeploy-commands-secure"
-#       private_dns_enabled = true
-#       subnet_ids          = module.vpc.private_subnets
-#     },
-#   }
+  endpoints = {
+    s3 = {
+      service = "s3"
+      tags    = { Name = "s3-vpc-endpoint" }
+    },
+    dynamodb = {
+      service         = "dynamodb"
+      service_type    = "Gateway"
+      route_table_ids = flatten([module.vpc.intra_route_table_ids, module.vpc.private_route_table_ids, module.vpc.public_route_table_ids])
+      policy          = data.aws_iam_policy_document.dynamodb_endpoint_policy.json
+      tags            = { Name = "dynamodb-vpc-endpoint" }
+    },
+    ssm = {
+      service             = "ssm"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_tls.id]
+    },
+    ssmmessages = {
+      service             = "ssmmessages"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    lambda = {
+      service             = "lambda"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecs = {
+      service             = "ecs"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecs_telemetry = {
+      create              = false
+      service             = "ecs-telemetry"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ec2 = {
+      service             = "ec2"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_tls.id]
+    },
+    ec2messages = {
+      service             = "ec2messages"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
+    },
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
+    },
+    kms = {
+      service             = "kms"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_tls.id]
+    },
+    codedeploy = {
+      service             = "codedeploy"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    codedeploy_commands_secure = {
+      service             = "codedeploy-commands-secure"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+  }
 
-#   tags = merge(local.tags, {
-#     Project  = "Secret"
-#     Endpoint = "true"
-#   })
-# }
+  tags = merge(local.tags, {
+    Project  = "Secret"
+    Endpoint = "true"
+  })
+}
 
-# module "vpc_endpoints_nocreate" {
-#   source = "../../modules/vpc-endpoints"
+module "vpc_endpoints_nocreate" {
+  source = "./modules/vpc-endpoints"
 
-#   create = false
-# }
+  create = false
+}
 
 
-# data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
-#   statement {
-#     effect    = "Deny"
-#     actions   = ["dynamodb:*"]
-#     resources = ["*"]
+data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
+  statement {
+    effect    = "Deny"
+    actions   = ["dynamodb:*"]
+    resources = ["*"]
 
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
-#     }
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
 
-#     condition {
-#       test     = "StringNotEquals"
-#       variable = "aws:sourceVpce"
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:sourceVpce"
 
-#       values = [module.vpc.vpc_id]
-#     }
-#   }
-# }
+      values = [module.vpc.vpc_id]
+    }
+  }
+}
 
-# data "aws_iam_policy_document" "generic_endpoint_policy" {
-#   statement {
-#     effect    = "Deny"
-#     actions   = ["*"]
-#     resources = ["*"]
+data "aws_iam_policy_document" "generic_endpoint_policy" {
+  statement {
+    effect    = "Deny"
+    actions   = ["*"]
+    resources = ["*"]
 
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
-#     }
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
 
-#     condition {
-#       test     = "StringNotEquals"
-#       variable = "aws:SourceVpc"
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:SourceVpc"
 
-#       values = [module.vpc.vpc_id]
-#     }
-#   }
-# }
+      values = [module.vpc.vpc_id]
+    }
+  }
+}
 
--- outputs.tf
+++ outputs.tf
@@ -534,8 +534,8 @@ output "vpc_flow_log_cloudwatch_iam_role_arn" {
   value       = module.vpc.vpc_flow_log_cloudwatch_iam_role_arn
 }
 
-# # VPC endpoints
-# output "vpc_endpoints" {
-#   description = "Array containing the full resource object and attributes for all endpoints created"
-#   value       = module.vpc_endpoints.endpoints
-# }
+# VPC endpoints
+output "vpc_endpoints" {
+  description = "Array containing the full resource object and attributes for all endpoints created"
+  value       = module.vpc_endpoints.endpoints
+}
